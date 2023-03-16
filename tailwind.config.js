95348e8f4331a7fdff36b507f28a981ee60eb5e4
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-03-16 21:06:02 +0000

    added header component with navbar, nav-items & search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
         'regal-blue': '#243c5a',
+        'dark-grey': '#393939',
+        tan: '#fceed1',
+        blondey: '#c2b490',
+        'Light-Classy-Grey': '#f1f0ee',
+        'Deep-Orange': '#FF5A09',
+        clean: '#DFDCE3',
+        mint: '#4cb69f',
       },
     },
   },
