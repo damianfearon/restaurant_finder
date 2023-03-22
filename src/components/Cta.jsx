5a06dadeba760838887e3945513d9e0fbcee1000
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-03-22 11:10:15 +0000

    added social media icons to footer

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default function Cta() {
    return (
-    // !-- CTA Section -->
+    // { /* CTA Section */ }
     <section id="cta" class="bg-regal-blue">
-      {/* <!-- Flex Container --> */}
+      {/*  Flex Container  */}
       <div
         class="container flex flex-col items-center justify-between px-6 py-24 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0"
       >
-        {/* <!-- Heading --> */}
+        {/*  Heading  */}
         <h2
           class="text-5xl font-bold leading-tight text-center text-white md:text-4xl md:max-w-xl md:text-left"
         >
-          Eat With Us Today!
+          Dine With Us Today!
         </h2>
         <div class="my-16">
           <button
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-
-
-
 export default function Footer() {
   return (
     <footer className='bg-mint'>
@@ -12,7 +9,7 @@ export default function Footer() {
         <div className='flex flex-col-reverse items-center justify-between space-y-12'
         >
           <div className='mx-auto my-6 text-center text-white'>
-            Restaurant Finder &copy; 2023, All Rights Reserved
+            Restaurant Finder &copy; 2023, All Rights Reserved.
            </div>
 
            {/* Logo */}
@@ -22,9 +19,32 @@ export default function Footer() {
           
         </div>
 
-        {/* List Container */}
-        <div class="flex justify-around space-x-32">
-          <div class="flex flex-col space-y-3 text-white">
+       {/* Social Links Container & Icons */}
+       <div class="flex justify-center space-x-4">
+
+       <a href="#">
+              <img src="/img/icon-facebook.svg" alt="" class="h-8" />
+            </a>
+            
+            {/* Twitter */}
+            <a href="#">
+              <img src="/img/icon-twitter.svg" alt="" class="h-8" />
+            </a>
+           
+            {/* Instagram */}
+            <a href="#">
+              <img src="/img/icon-instagram.svg" alt="" class="h-8" />
+            </a>
+
+            {/* YouTube */}
+            <a href="#">
+              <img src="/img/icon-youtube.svg" alt="" class="h-8" />
+            </a>
+           </div>
+
+          {/* List Container */}
+        <div className="flex justify-around space-x-32">
+          <div className="flex flex-col space-y-3 text-white">
             <a href="#" className='hover:text-regal-blue'>Home</a>
             <a href="#" className='hover:text-regal-blue'>Restaurants</a>
             <a href="#" class="hover:text-regal-blue">Locations</a>
@@ -33,13 +53,11 @@ export default function Footer() {
           <div class="flex flex-col space-y-3 text-white">
             <a href="#" class="hover:text-regal-blue">Careers</a>
             <a href="#" class="hover:text-regal-blue">Corporate Social Responsibility</a>
+            <a href="#" class="hover:text-regal-blue">Green Facts</a>
             <a href="#" class="hover:text-regal-blue">Privacy Policy</a>
           </div>
         </div>
-
-      </div>
-
-  
-    </footer>
+ </div>
+ </footer>
   )
 }
