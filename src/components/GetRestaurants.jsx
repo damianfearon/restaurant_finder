45fbedd073999738012f2106081f0f7e757a13b8
Mobolaji Kuti <mobolajiaokuti@gmail.com>
2023-03-23 04:41:27 +0000

    Added comments to GetRestaurants.jsx component

--- src/components/GetRestaurants.jsx
+++ src/components/GetRestaurants.jsx
@@ -1,3 +1,4 @@
+// Importing required libraries
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import {motion} from "framer-motion";
@@ -5,8 +6,12 @@ import {motion} from "framer-motion";
 //include import from another js file for location to coordinates
 
 //const axios = require('axios').default
+
+// Defining the GetRestaurants functional component that takes in coordinates as a prop
 const GetRestaurants = ({coordinates}) => {
+  // Setting the initial state of restaurants to an empty array
   const [restaurants, setRestaurants] = useState ([])
+    // Using the useEffect hook to make an API call using axios and the RapidAPI service
     useEffect (() => {
       if (coordinates.lat && coordinates.lon) {
         const options = {
@@ -27,22 +32,27 @@ const GetRestaurants = ({coordinates}) => {
             'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
           }
         };
+        // Making the API call using axios, filtering the reponse data to include only restaurants with a name, and setting the state using setRestaurants
         axios.request(options).then(function (response) {
+          // Logging the response data to the console
           console.log(response.data)
+          // Setting the state of restaurants to the filtered data
           const filteredData = response.data.data.filter((restaurant) => restaurant.name )
           setRestaurants(filteredData)
+          // Logging any errors to the console
         }).catch(function (error) {
           console.error(error);
         });
       }
     }, [coordinates])
 
+  // Defining the animation effects using motion variants
   const cardVariant = {
     hidden: {opacity:0},
     visible: {opacity:1},
   }
   
-
+  // Returning the JSX code to display the list of restaurants
   return (
     <div className="p-16 bg-blue-100">
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
