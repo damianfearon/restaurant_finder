3436f12c2ab7d225f6663cf1b31c49b88fe87ad2
Mobolaji Kuti <mobolajiaokuti@gmail.com>
2023-03-23 04:23:12 +0000

    Added comments to API_postcode.jsx component

--- src/components/API_postcode.jsx
+++ src/components/API_postcode.jsx
@@ -1,60 +1,59 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 
-
-//include import from another js file for location to coordinates
-
-//const axios = require('axios').default
 function APIpostcode ({searchLocation, setCoordinatesCallback}) {
-const [coordinates, setCoordinates] = useState ({lat: null, lon: null})
-const [debouncedLocation, setDebouncedLocation] = useState (searchLocation)
-useEffect (() => {
-  const timerId = setTimeout(() => {
-    setDebouncedLocation (searchLocation);
-  }, 500);
-  return () => {
-    clearTimeout(timerId)
-  }
-}, [searchLocation, setCoordinates]) 
-useEffect (() => {
-  if (debouncedLocation) {
-    const options = {
-      method: 'GET',
-      url: 'https://forward-reverse-geocoding.p.rapidapi.com/v1/forward',
-      params: {
-        street: searchLocation, //19/03/23 - add searchLocation variable
-        // city: 'New York Cit',
-        // state: 'NY',
-        // postalcode: '10011',
-        // country: 'USA',
-        'accept-language': 'en',
-        polygon_threshold: '0.0'
-      },
-      headers: {
-        'X-RapidAPI-Key': '96d3145458msh88398ed9642d141p10ef6ajsne53e90f42611',
-        'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com'
-      }
-    };
+  // Define two state variables: coordinates and debouncedLocation
+  const [coordinates, setCoordinates] = useState ({lat: null, lon: null})
+  const [debouncedLocation, setDebouncedLocation] = useState (searchLocation)
+
+  // useEffect hook to update debouncedLocation after 500ms delay when searchLocation is changed
+  useEffect (() => {
+    const timerId = setTimeout(() => {
+      setDebouncedLocation (searchLocation);
+    }, 500);
+    // Return a cleanup function to clear the timeout when the component unmounts or searchLocation or setCoordinates is changed
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [searchLocation, setCoordinates]) 
+
+  // useEffect hook to send a GET request to fetch coordinates for the debouncedLocation
+  useEffect (() => {
+    // Check if debouncedLocation exists
+    if (debouncedLocation) {
+      // Define the options object to specify the URL, parameters, and headers for the API request
+      const options = {
+        method: 'GET',
+        url: 'https://forward-reverse-geocoding.p.rapidapi.com/v1/forward',
+        params: {
+          street: searchLocation,
+          'accept-language': 'en',
+          polygon_threshold: '0.0'
+        },
+        headers: {
+          'X-RapidAPI-Key': '96d3145458msh88398ed9642d141p10ef6ajsne53e90f42611',
+          'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com'
+        }
+      };
   
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-       setCoordinatesCallback ({
-        lat: response.data[0].lat,
-        lon: response.data[0].lon
-       }) 
-  }).catch(function (error) {
-    console.error(error);
-  });
+      // Send the API request using axios library
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        // If the request is successful, call the setCoordinatesCallback function to pass the latitude and longitude values to the parent component
+        setCoordinatesCallback ({
+          lat: response.data[0].lat,
+          lon: response.data[0].lon
+        }) 
+      }).catch(function (error) {
+        // If there is an error, log the error to the console
+        console.error(error);
+      });
+    }
+  }, [debouncedLocation])
+
+  
+  return (<></>)
 }
-}, [debouncedLocation])
 
-
-
-
-
-
-
-return (<></>)
-
-}
+// Export the component as the default export
 export default APIpostcode
